7cc59c9725be15d41071c03e91b31700c610a081
zeratul117 <luisbelis@hotmail.com>
2025-03-17 12:21:47 -0400

    adding PDF and some css color changes

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -5,12 +5,13 @@ export default function Footer() {
   return (
     <footer className="bg-[#224081] rounded-lg shadow-sm m-4 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <button
-          type="button"
+        <a
+          href="/Luis_Belis_CV.pdf"
+          download="Luis_Belis_CV.pdf"
           className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
           Download CV
-        </button>
+        </a>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li className="whiteColor">
             <Link target="_blank" href="https://www.linkedin.com/in/luis-belis-755426186/">
@@ -26,4 +27,4 @@ export default function Footer() {
       </div>
     </footer>
   );
-};
+}
